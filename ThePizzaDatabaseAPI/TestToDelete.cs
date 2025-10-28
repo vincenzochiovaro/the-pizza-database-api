62f04a0127b7fb6034ac4c7abbbeaf925ceaf0a6
Vincenzo Chiovaro <vincenzochiovaro194@gmail.com>
2025-10-28 20:37:25 +0000

    create test azure function)

---
+++ ThePizzaDatabaseAPI/TestToDelete.cs
@@ -0,0 +1,25 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ThePizzaDatabaseAPI;
+
+public class TestToDelete
+{
+    private readonly ILogger<TestToDelete> _logger;
+
+    public TestToDelete(ILogger<TestToDelete> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("TestToDelete")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+        
+    }
+
+}
