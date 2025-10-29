57f0c6262c7b9e06d20686befcea478022d4d855
Vincenzo Chiovaro <vincenzochiovaro194@gmail.com>
2025-10-29 21:06:23 +0000

    feat: add API key authentication middleware for HTTP-triggered functions

--- ThePizzaDatabaseAPI/Program.cs
+++ ThePizzaDatabaseAPI/Program.cs
@@ -2,10 +2,12 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ThePizzaDatabaseAPI;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
+builder.UseMiddleware<ApiKeyMiddleware>();
 
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
