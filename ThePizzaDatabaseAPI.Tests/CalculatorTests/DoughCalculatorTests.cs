75f34be22ad22de68c9b6901845f014192019c25
Vincenzo Chiovaro <vincenzochiovaro194@gmail.com>
2026-04-13 22:06:07 +0100

    lint

--- ThePizzaDatabaseAPI.Tests/CalculatorTests/DoughCalculatorTests.cs
+++ ThePizzaDatabaseAPI.Tests/CalculatorTests/DoughCalculatorTests.cs
@@ -7,8 +7,5 @@ public class DoughCalculatorTests
     [Fact]
     public void when_type_is_biga_then_salt_on_day1_is_null()
     {
-        var sut = DoughCalculator.Calculate("Biga", 0,0,0,0,0);
-        
-        Assert.Null(sut.Salt);
     }
 }
